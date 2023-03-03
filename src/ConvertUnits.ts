8e745af59833b53261b8ae619ee46bf5d67fe599
Luciekimotho <luciekimotho@gmail.com>
2023-03-03 14:10:09 +0300

    add convert units function for weight

--- src/ConvertUnits.ts
+++ src/ConvertUnits.ts
@@ -1,4 +1,4 @@
-export const ConvertUnits = (value: number, from: string, to: string ) => {
+export const ConvertVolumeUnits = (value: number, from: string, to: string ) => {
 
     const units: any = {
         ml: {
@@ -20,3 +20,23 @@ export const ConvertUnits = (value: number, from: string, to: string ) => {
 
     return value * units[from].ratio / units[to].ratio;
 };
+
+export const ConvertWeightUnits = (value: number, from: string, to: string ) => {
+    
+        const units: any = {
+            g: {
+                ratio: 1
+            },
+            kg: {
+                ratio: 1000
+            },
+            oz: {
+                ratio: 28.3495
+            },
+            lb: {
+                ratio: 453.592
+            },
+        }
+    
+        return value * units[from].ratio / units[to].ratio;
+}
